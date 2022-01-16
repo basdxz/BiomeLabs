fd2a04a2c9e44a294ff95e869e7fd6e1868c86ba
basdxz <tudurap.com@gmail.com>
2022-01-16 19:35:26 +0000

    Initial commit

---
+++ src/main/java/com/basdxz/biomelabs/BiomeLabs.java
@@ -0,0 +1,12 @@
+package com.basdxz.biomelabs;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = Tags.MODID, version = Tags.VERSION, name = Tags.MODNAME, acceptedMinecraftVersions = "[1.7.10]")
+public class BiomeLabs {
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent e1vent) {
+        System.out.println("BiomeLabs Loaded!");
+    }
+}
